05f5a1157ae60c536a49f56b142725f21bdf4791
Hassan <hassan.ali.36900@outlook.com>
2021-11-05 14:12:15 +0200

    done

--- Product.cpp
+++ Product.cpp
@@ -12,14 +12,14 @@ bool ProductInfo::operator==(int i)const {
 	return (Parcode == i ? true : false);
 }
 
+ostream& operator<<(ostream& output, ProductInfo& rhs) {
+	return output << '#' << setfill('0') << setw(12) << rhs.Parcode << "\t " << rhs.Name;
+}
 
 ostream& operator<<(ostream& output, ProductInfo_p& rhs) {
 	return output << '#' << setfill('0') << setw(12) << rhs->Parcode << "\t " << rhs->Name;
 }
 
-ostream& operator<<(ostream& output, ProductInfo& rhs) {
-	return output << '#' << setfill('0') << setw(12) << rhs.Parcode << "\t " << rhs.Name;
-}
 
 ProductNode::ProductNode() :Info(NULL), Next(NULL), Amount(0) {
 	;
